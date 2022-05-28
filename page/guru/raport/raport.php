0e8316939e4f4c769e111d257723bcdfa8447f12
buburonta <lah.ontaks@gmail.com>
2022-05-29 00:56:31 +0700

    Customize raport using datatables

--- page/guru/raport/raport.php
+++ page/guru/raport/raport.php
@@ -14,8 +14,8 @@ $kelas = query("SELECT * FROM kelas");
 $jurusan = query("SELECT * FROM jurusan");
 $ujian = query("SELECT * FROM daftar_ujian WHERE id_guru='$guru[NIP]'");
 $status = [
-    ["id_status"=>"selesai","status"=>"Selesai"],
-    ["id_status"=>"belum submit","status"=>"Belum selesai"]
+    ["id_status" => "selesai", "status" => "Selesai"],
+    ["id_status" => "belum submit", "status" => "Belum selesai"]
 ];
 
 // --- Filter
@@ -58,7 +58,7 @@ if (isset($_POST['filter'])) {
 }
 
 // -- SEarch
-if(isset($_POST['search'])) {
+if (isset($_POST['search'])) {
     $nama = htmlspecialchars($_POST['nama']);
     // echo $nama;
     $daftarUjian = query("SELECT * FROM murid_ujian INNER JOIN daftar_ujian ON murid_ujian.id_ujian=daftar_ujian.id_ujian INNER JOIN kelas ON murid_ujian.kelas=kelas.id_kelas INNER JOIN jurusan ON murid_ujian.jurusan=jurusan.id_jurusan INNER JOIN users ON murid_ujian.id_murid=users.id_user WHERE daftar_ujian.id_guru='$guru[NIP]' AND users.nama='$nama' OR users.nama LIKE '%$nama%' ORDER BY id DESC");
@@ -73,149 +73,21 @@ if(isset($_POST['search'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Raport</title>
     <link rel="stylesheet" href="assets/css/guru/raport.css">
-    <style>
-        div.container div.center {
-            width: 900px;
-            max-height: 450px;
-            overflow: auto;
-            /* padding: 0 20px; */
-            padding: 30px 10px;
-            padding-top: 0;
-            overflow-x: hidden;
-            /* flex-direction: column; */
-            /* align-items: center; */
-            /* justify-content: flex-start; */
-        }
-
-        div.center a.keluar {
-            font-size: 30px;
-            color: #fff;
-            position: absolute;
-            left: 20px;
-            top: 15px;
-            transform: rotate(180deg);
-            z-index: 99;
-        }
-
-        div.center a.keluar:hover {
-            color: #ddd;
-        }
-
-        div.content {
-            position: relative;
-            width: 700px;
-            margin-top: 20px;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-        }
-
-        .sorter {
-            top: 25px;
-            color: #333;
-        }
-        .sorter button, .searchNama button {
-            padding: 0 5px;
-        }
-        .searchNama {
-            color: #333;
-            margin: 5px 0;
-        }
-        .searchNama input, .sorter input {
-            border-radius: 5px;
-            border: none;
-            padding: 1px 0;
-            padding-left: 10px;
-            font-size: 12px;
-            /* outline: none; */
-            font-family: "Poppins", sans-serif;
-        }
-    </style>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
 </head>
 
 <body>
     <?php include_once $nav ?>
-    <style>
-        nav.head {
-            color: #fff;
-        }
 
-        nav.head .left img {
-            filter: none;
-        }
-
-        nav.head div.center h1 {
-            color: #fff;
-            font-style: normal;
-            font-weight: 300;
-            font-size: 21px;
-            letter-spacing: 0.02em;
-            color: #FFFFFF;
-        }
-    </style>
-    <div class="container">
-        <div class="center">
+    <div class="center">
+        <div class="container">
             <a href="?page=murid" class="keluar">
-                <i class="fa-solid fa-right-to-bracket"></i>
+                <!-- <i class="fa-solid fa-right-to-bracket"></i> -->
+                Kembali
             </a>
 
             <div class="content">
-                <div class="sorter">
-                    <form method="POST" action="">
-                        <span>Filter By : </span>
-                        <select name="kelas" id="kelas">
-                            <option value="" hidden selected>Kelas</option>
-                            <?php foreach ($kelas as $kel): ?>
-                            <option
-                                value="<?=$kel['id_kelas']?>">
-                                <?= $kel['kelas'] ?>
-                            </option>
-                            <?php endforeach; ?>
-                        </select>
-
-                        <select name="jurusan" id="jurusan">
-                            <option value="" hidden selected>Jurusan</option>
-                            <?php foreach ($jurusan as $jur): ?>
-                            <option
-                                value="<?= $jur['id_jurusan'] ?>">
-                                <?= $jur['id_jurusan'] ?>
-                            </option>
-                            <?php endforeach; ?>
-                        </select>
-
-                        <select name="ujian" id="ujian">
-                            <option value="" hidden selected>Ujian</option>
-                            <?php foreach ($ujian as $uji): ?>
-                            <option
-                                value="<?= $uji['id_ujian'] ?>">
-                                <?= $uji['judul'] ?>
-                            </option>
-                            <?php endforeach; ?>
-                        </select>
-
-                        <select name="status" id="status">
-                            <option value="" hidden selected>Status</option>
-                            <?php foreach ($status as $stat): ?>
-                            <option
-                                value="<?= $stat['id_status'] ?>">
-                                <?= $stat['status'] ?>
-                            </option>
-                            <?php endforeach; ?>
-                        </select>
-
-                        <button type="submit" name="filter">Filter</button>
-                    </form>
-                </div>
-
-                <div class="searchNama">
-                    <form method="POST" action="">
-                        <label for="nama">Nama</label>
-                        <input type="text" name="nama" id="nama" placeholder="Cari peserta">
-                        <button type="submit" name="search">Cari</button>
-                    </form>
-                </div>
-                <!-- <div id="table-reports"> -->
-                <table cellspacing='0' id="table-raport">
+                <table cellspacing='0' id="table-raport" class="table table-striped table-bordered data">
                     <thead>
                         <th>No</th>
                         <th>Mata Pelajaran</th>
@@ -228,51 +100,45 @@ if(isset($_POST['search'])) {
                         <th>Waktu Selesai</th>
                     </thead>
 
-                    <?php //if(count($daftarUjian) > 10){?>
-                    <style>
-                        div.container div.center {
-                            /* justify-content: flex-start; */
-                            align-items: flex-start;
-                            /* padding: 30px 10px; */
-                            /* padding-top: 60px; */
-                        }
-                    </style>
-                    <?php //}?>
-                    <div id="tableValue">
                     <?php $no = 1; ?>
-                    <?php foreach ($daftarUjian as $ujian) { ?>
                     <tbody>
-                        <td><?= $no ?>
-                        </td>
-                        <td><?= $ujian['judul'] ?>
-                        </td>
-                        <td><?= $ujian['kelas'] ?>
-                        </td>
-                        <td><?= $ujian['jurusan'] ?>
-                        </td>
-                        <td><?= $ujian['nama'] ?>
-                        </td>
-                        <td><?= $ujian['nilai'] ?>
-                        </td>
-                        <td><?= $ujian['predikat'] ?>
-                        </td>
-                        <td><?= $ujian['tipe_ujian'] ?>
-                        </td>
-                        <td><?= $ujian['waktu_selesai'] ?>
-                        </td>
+                        <?php foreach ($daftarUjian as $ujian) { ?>
+                            <tr>
+                                <td><?= $no ?>
+                                </td>
+                                <td><?= $ujian['judul'] ?>
+                                </td>
+                                <td><?= $ujian['kelas'] ?>
+                                </td>
+                                <td><?= $ujian['jurusan'] ?>
+                                </td>
+                                <td><?= $ujian['nama'] ?>
+                                </td>
+                                <td><?= $ujian['nilai'] ?>
+                                </td>
+                                <td><?= $ujian['predikat'] ?>
+                                </td>
+                                <td><?= $ujian['tipe_ujian'] ?>
+                                </td>
+                                <td><?= $ujian['waktu_selesai'] ?>
+                                </td>
+                            </tr>
+                        <?php $no++;
+                        } ?>
                     </tbody>
-                    <?php $no++;
-                    } ?>
-                    </div>
                 </table>
-                <!-- </div> -->
             </div>
         </div>
     </div>
-    <script src="assets/js/liveSearch.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
     <script>
-        liveSearch('nama','#tableValue', 'raportGuru.php')
+        $(document).ready(function() {
+            $('.data').DataTable();
+        });
     </script>
+
 </body>
 
 </html>
--- page/murid/raport/raport.php
+++ page/murid/raport/raport.php
@@ -18,7 +18,9 @@ $daftarUjian = query("SELECT * FROM murid_ujian INNER JOIN daftar_ujian ON murid
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Raport</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
     <link rel="stylesheet" href="assets/css/murid/raport.css">
+
     <style>
         .logo {
             position: absolute;
@@ -32,56 +34,53 @@ $daftarUjian = query("SELECT * FROM murid_ujian INNER JOIN daftar_ujian ON murid
     <div class="logo">
         <img src="assets/img/cutest_logo_text.svg" onclick="window.location.href = '?page=murid';">
     </div>
-    <div class="container">
-        <div class="center">
+    <div class="center">
+        <div class="container">
             <a href="?page=murid" class="keluar">
                 <i class="fa-solid fa-right-to-bracket"></i>
             </a>
-            <style>
-                div.container a.keluar {
-                    font-size: 30px;
-                    color: #fff;
-                    position: absolute;
-                    left: 25px;
-                    top: 18px;
-                    transform: rotate(180deg);
-                    z-index: 99;
-                }
-                div.container a.keluar:hover {
-                    color: #ddd;
-                }
-            </style>
 
-                <h1>
-                    <?= $users['nama'] ?> | <?= $users['kelas'] ?> <span style="text-transform:uppercase;"><?= $users['id_jurusan'] ?></span> | <?= $users['jurusan'] ?>
-                </h1>
+            <h1>
+                <?= $users['nama'] ?> | <?= $users['kelas'] ?> <span style="text-transform:uppercase;"><?= $users['id_jurusan'] ?></span> | <?= $users['jurusan'] ?>
+            </h1>
             <div class="content">
-                <table cellspacing='0'>
-                    <thead>
-                        <th>No</th>
-                        <th>Mata Pelajaran</th>
-                        <th>Angka</th>
-                        <th>Prediket</th>
-                        <th>Ket.</th>
-                        <th>Waktu Selesai</th>
-                    </thead>
+            <table class="table table-striped table-bordered data">
+                <thead>
+                    <th>No</th>
+                    <th>Mata Pelajaran</th>
+                    <th>Angka</th>
+                    <th>Prediket</th>
+                    <th>Ket.</th>
+                    <th>Waktu Selesai</th>
+                </thead>
 
-                    <?php $no = 1; ?>
+                <?php $no = 1; ?>
+                <tbody>
                     <?php foreach ($daftarUjian as $ujian) { ?>
-                        <tbody>
+                        <tr>
                             <td><?= $no ?></td>
                             <td><?= $ujian['judul'] ?></td>
                             <td><?= $ujian['nilai'] ?></td>
                             <td><?= $ujian['predikat'] ?></td>
                             <td><?= $ujian['keterangan'] ?></td>
                             <td><?= $ujian['waktu_selesai'] ?></td>
-                        </tbody>
+                        </tr>
                     <?php $no++;
                     } ?>
-                </table>
+                </tbody>
+            </table>
             </div>
         </div>
     </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('.data').DataTable();
+        });
+    </script>
 </body>
 
 </html>
